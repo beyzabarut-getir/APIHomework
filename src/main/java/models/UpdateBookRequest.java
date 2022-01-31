5ec8383fc69b80a60c810fa0979588634beeafaf
Beyza <beyza.barut@getir.com>
2022-01-31 23:06:15 +0300

    all changes final

---
+++ src/main/java/models/UpdateBookRequest.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.Map;
+
+public class UpdateBookRequest {
+    public String firstname;
+    public String lastname;
+    public Number totalprice;
+    public Boolean depositpaid;
+    public Map<String, String> bookingdates;
+    public String additionalneeds;
+
+    public UpdateBookRequest(String firstname, String lastname, Number totalprice, Boolean depositpaid, Map<String, String> bookingdates, String additionalneeds) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.totalprice = totalprice;
+        this.depositpaid = depositpaid;
+        this.bookingdates = bookingdates;
+        this.additionalneeds = additionalneeds;
+    }
+}
